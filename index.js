be238c78d0398b1a96ab15cdcf8dbb3ef7389fae
Lennard <Lennard.r@hotmail.de>
2017-10-17 15:20:59 +0200

    A System like Pub/Sub was implementet!

--- index.js
+++ index.js
@@ -1,46 +1,23 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const AudioPlayer = require("./audio/AudioMaster.js");
-const Bot = require('./bot/bot.js');
-
 const config = require("./config.json");
-
-var prefixs = [];
-var fnc = {};
+const moduleLoader = require('./module/ModuleLoader.js');
 
 client.on("ready", () => {
-  player = new AudioPlayer(client);
-  bot = new Bot();
-
-  fnc[player.prefix] = player;
-  fnc[bot.prefix] = bot;
-  prefixs.push(bot.prefix);
-  prefixs.push(player.prefix);
-  
-    console.log("ready");
+  moduleLoader.loadModules(client);
+  console.log("ready");
 });
 
-
 client.on("message", message => {
+  // bot reagieren nicht auf bots
   if(message.author.bot) return;
-
-
-  for(let i = 0; i < prefixs.length; i++){
-    if(message.content.indexOf(prefixs[i]) === 0){
-      message.content = message.content.slice(prefixs[i].length).trim();
-      const args = message.content.split(/ +/g);
-      fnc[prefixs[i]].onMessage(message, args);
-
-      message.delete();
-      break;
-    }
-  }
-
+  
+  moduleLoader.onAction("message", {message});
 });
 
 client.on("voiceStateUpdate", (oldMember, newMember) => {
-  player.onVoiceStateChange(oldMember, newMember);
+  moduleLoader.onAction("voiceStateUpdate", {oldMember, newMember});
 });
 
 // login
